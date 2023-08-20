408082423d30d9cb8c6d82ad83eda29359174fe3
shizuku0714 <42108175+shizuku0714@users.noreply.github.com>
2023-08-21 00:33:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,5 +19,5 @@ def hello_http(request):
     elif request_args and 'name' in request_args:
         name = request_args['name']
     else:
-        name = 'Build'
+        name = 'Build２'
     return 'Hello {}!'.format(name)
